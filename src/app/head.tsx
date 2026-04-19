3487d74afa7435741555b44d364204261535f5a7
Ram Karan Kumar <150804410+rkaran2558@users.noreply.github.com>
2026-04-19 18:15:53 +0530

    Add Head component with Google Ads script

---
+++ src/app/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2200445871437905"
+        crossOrigin="anonymous"
+      />
+    </>
+  )
+}
